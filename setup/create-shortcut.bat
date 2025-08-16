f7e18f89ed551f5baea64664cc275aa80c4c2d9e
Konrad Czumbil <czkonrad88@gmail.com>
2025-08-16 22:10:22 +0200

    Update create-shortcut.bat
    Due to the running of start.bat, there was always an extra window opening at startup, I fixed this bug.

--- setup/create-shortcut.bat
+++ setup/create-shortcut.bat
@@ -1,7 +1,7 @@
 @echo off
 set "APP_NAME=NextShell Terminal"
 set "SCRIPT_DIR=%~dp0"
-set "TARGET_BAT=%SCRIPT_DIR%..\start.bat"
+set "TARGET_BAT=%SCRIPT_DIR%..\start.vbs"
 set "DESKTOP_PATH=%USERPROFILE%\Desktop"
 set "SHORTCUT_PATH=%DESKTOP_PATH%\%APP_NAME%.lnk"
 echo Set oWS = WScript.CreateObject("WScript.Shell") > "%TEMP%\create_shortcut.vbs"
@@ -9,8 +9,9 @@ echo Set oLink = oWS.CreateShortcut("%SHORTCUT_PATH%") >> "%TEMP%\create_shortcu
 echo oLink.TargetPath = WScript.CreateObject("Scripting.FileSystemObject").GetAbsolutePathName("%TARGET_BAT%") >> "%TEMP%\create_shortcut.vbs"
 echo oLink.WorkingDirectory = WScript.CreateObject("Scripting.FileSystemObject").GetParentFolderName(WScript.CreateObject("Scripting.FileSystemObject").GetAbsolutePathName("%TARGET_BAT%")) >> "%TEMP%\create_shortcut.vbs"
 echo oLink.Save >> "%TEMP%\create_shortcut.vbs"
-cscript //nologo "%TEMP%\create_shortcut.vbs"
+cscript "%TEMP%\create_shortcut.vbs"
 del "%TEMP%\create_shortcut.vbs"
 echo.
 echo New shortcut created: "%APP_NAME%"
 pause
+
